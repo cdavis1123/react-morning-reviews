7da1204e1838ae2a81406f41b4c7e11246ae1161
cdavis85 <christydavis1123@gmail.com>
2020-10-21 10:58:32 -0500

    "first react review"

--- src/App.js
+++ src/App.js
@@ -1,1 +1,42 @@
 
+import { render } from '@testing-library/react'
+import React, {Component} from 'react';
+import List from './components/List'
+import './App.css'
+
+
+
+//class App extends Component{
+  //  constructor(){
+    //    super()
+      //  this.state = {
+//
+  //      }
+    //}
+
+
+
+//render(){
+  //  return(
+    //    <div className='App'>
+      //      <h1 className='movie-list-title'>The Most Amazing Movie List</h1>
+        //</div>
+    //);
+//}
+
+
+//}
+
+function App(){
+
+    return(
+        <div className='App' id='my-id'>
+            <h1 className='move-list-title'>The Most Amazing Movie List</h1>
+        </div>
+    );
+}
+
+
+
+
+export default App;
